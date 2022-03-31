09776d6393c708b5c0badf635fdc89f61dd3710d
Aaron Hardowar <hardowar.aaron@gmail.com>
2022-03-31 12:33:22 -0400

    Setup React frontend and Spring Boot Back End

---
+++ src/main/java/com/hardowar/fullfitness/FullFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.hardowar.fullfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullFitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardowar/fullfitness/FullFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardowar.fullfitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
